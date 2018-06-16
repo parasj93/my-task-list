099b4193cded8d6ddd95d7578802ca71b79d8f8d
parasj93 <parasjain93@gmail.com>
2018-06-16 09:15:02 +0530

    adding the client folder

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,36 +1,38 @@
+var _ = require('lodash');
 var express = require('express');
+var { ObjectId } = require('mongodb');
 var router = express.Router();
-var tasks = require('../models/task.model');
+var Tasks = require('../models/task.model');
 
-//all task
-router.get('/tasks', (req, res, next) => {
-    tasks.find((err, data) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(data);
+//GET all task
+router.get('/tasks', (req, res) => {
+    Tasks.find().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status.send(err);
     })
 })
 
-//only 1 task
+//GET only One task
 router.get('/task/:id', (req, res, next) => {
     console.log(req.params.id);
-    tasks.findOne({ _id: req.params.id = req.params.id }, (err, test) => {
-        if (err) {
-            res.send(err);
-        }
+    var id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        res.status(404).send('Invalid Object Id');
+    }
 
-        res.json(test);
+    Tasks.findById(id).then((doc) => {
+        res.send(doc);
+    }).catch((e) => {
+        res.status(400).send(e);
     })
-
 })
 
-//save a task
+//POST a new task
 router.post('/task', (req, res, next) => {
-    var task = new tasks(req.body);
+    var task = new Tasks(req.body);
     console.log('newatask', task);
-    //console.log(task.title + '     ' + task.isDone   );
+    //console.log(task.title + '     ' + task.isDone);
     // if (!task.title || !task.isDone + '') {
     //     res.status(400);
     //     res.json({
@@ -45,19 +47,18 @@ router.post('/task', (req, res, next) => {
     //         res.json(data);
     //     })
     // }
-    task.save((err, data) => {
-        if (err) {
-            res.send(err);
-            //console.log('error response', err); 
-        }
-        res.json(data);
+    task.save((data) => {
+        //console.log(data);
+        res.send(data);
+    }, (err) => {
+        res.status(400).send(err);
     })
 });
 
-//delete task
-
+//DELETE the One task
 router.post('/task/:id', (req, res, next) => {
-    tasks.remove({ _id: req.params.id = req.params.id }, (err, data) => {
+
+    Tasks.remove({ _id: req.params.id = req.params.id }, (err, data) => {
         if (err) {
             res.send(err)
         }
@@ -65,23 +66,37 @@ router.post('/task/:id', (req, res, next) => {
     })
 });
 
-//update task
+//UPDATE the existing task
 router.put('/task/:id', (req, res, next) => {
 
     //var task = JSON.stri  ngify(req.);
-    console.log('task is '+task);
-    console.log('   id is '+req.params.id);
+    //console.log('task is ' + task);
+    //console.log('   id is ' + req.params.id);
     //console.log('_id is '+req.params._id);
+    var id = req.params.id;
+    var body = _.pick(req.body, ['title', 'isDone']);
 
-    tasks.findByIdAndUpdate({ _id:req.params.id = req.params.id }, task, (err, res) => {
-        console.log('error response', err); 
-        console.log('data response - 1', res); 
-        if (err) {
-            //console.log(err)
-            console.log('error response', err); 
-        };
-        console.log('data response -2 ', res); 
+    // body.title = req.body.title;
+    // body.isDone = req.body.isDone;
+    if (id) {
+        console.log('body is what ' + JSON.stringify(body));
+    }
+
+    if (!ObjectId.isValid(id)) {
+        res.status(404).send('Invalid Object Id');
+    }
+
+    //body.title = req.body.title;
+    //body.isDone = req.body.isDone;
+
+    Tasks.findByIdAndUpdate(id, { $set: req.body }, { new: true }).then((doc) => {
+        console.log(doc);
+        if (!doc) {
+            res.status(404).send();
+        }
+        res.send(doc);
+    }).catch((err) => {
+        res.status(400).send();
     })
-
 })
 module.exports = router;
